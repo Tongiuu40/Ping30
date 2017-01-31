f8cf66922841802b3a215044c379539b34872223
shi congyi <tongu40@163.com>
2017-01-31 18:18:58 +0800

    ssss

--- ping30/src/examples.java
+++ ping30/src/examples.java
@@ -21,7 +21,7 @@ public class examples {
 	/**
 	 * Launch the application.
 	 */
-	public static void main(String[] args) {
+	/*public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
@@ -33,7 +33,7 @@ public class examples {
 			}
 		});
 	}
-
+*/
 	/**
 	 * Create the application.
 	 */
--- ping30/src/mycpptest.cpp
+++
@@ -1,8 +0,0 @@
-#include<iostream.h>
-#include"interfacetocpp.h"
-
-JNIEXPORT void JNICALL Java_interfacetocpp_connais(JNIEnv *env, jobject obj)
-{
-
-    cout<<"hello world"<<endl;
-}
